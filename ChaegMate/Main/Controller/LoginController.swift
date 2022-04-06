4addf78a915919316b835a7e8b879cfb3216b526
munyong <gjansdyd@gmail.com>
2022-04-06 12:15:23 +0900

    init

---
+++ ChaegMate/Main/Controller/LoginController.swift
@@ -0,0 +1,12 @@
+//
+//  LoginController.swift
+//  ChaegMate
+//
+//  Created by linkshops on 2022/04/06.
+//
+
+import Foundation
+
+class LoginController {
+    
+}
---
+++ ChaegMate/Main/View/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  ChaegMate
+//
+//  Created by munyong on 2022/04/06.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
